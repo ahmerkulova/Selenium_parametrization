91b7f750adfe0c02296a3326cbcec01a4f024402
Arina <ahmerkulova@gmail.com>
2022-05-06 16:29:31 +0300

    add assertion

--- test_items.py
+++ test_items.py
@@ -7,5 +7,6 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 def test_add_to_basket_button_should_be_available(browser):
     browser.get(link)
     browser.implicitly_wait(5)
-    browser.find_element(By.CLASS_NAME, 'btn-add-to-basket')
+    button = browser.find_element(By.CLASS_NAME, 'btn-add-to-basket')
+    assert button, 'No "Add to cart button" was found'
     sleep(5)  # time to check the button name
