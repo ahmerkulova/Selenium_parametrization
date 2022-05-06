fbd7a6d55d4c552bb35c7add0350ce8e888126dd
Arina <ahmerkulova@gmail.com>
2022-05-06 11:51:27 +0300

    add config for firefox

--- conftest.py
+++ conftest.py
@@ -1,12 +1,26 @@
 import pytest
 from selenium import webdriver
 
-@pytest.fixture(scope='function")
-def browser():
-    print('\nstarting browser')
-    options = webdriver.ChromeOptions()
-    options.add_experimental_option('excludeSwitches', ['enable-logging'])
-    browser = webdriver.Chrome(options=options)
+
+def pytest_addoption(parser):
+    parser.addoption('--browser_name', action='store', default='chrome',
+                     help='Enter browser option: chrome, firefox')
+
+
+@pytest.fixture(scope='function')
+def browser(request):
+    browser_name = request.config.getoption('browser_name')
+    browser = None
+    if browser_name == 'chrome':
+        print('\nstarting Chrome browser')
+        options = webdriver.ChromeOptions()
+        options.add_experimental_option('excludeSwitches', ['enable-logging'])
+        browser = webdriver.Chrome(options=options)
+    elif browser_name == 'firefox':
+        print('\nstarting Firefox browser')
+        browser = webdriver.Firefox()
+    else:
+        raise pytest.UsageError('--browser_name should be chrome or firefox')
     yield browser
     print('\nquiting browser')
     browser.quit()
