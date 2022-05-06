d1af4d9437695315c069a614ea6fd8544abe1d61
Arina <ahmerkulova@gmail.com>
2022-05-06 12:44:01 +0300

    add language config for conftest, define function to check button availability

--- conftest.py
+++ conftest.py
@@ -4,23 +4,30 @@ from selenium import webdriver
 
 def pytest_addoption(parser):
     parser.addoption('--browser_name', action='store', default='chrome',
-                     help='Enter browser option: chrome, firefox')
-
+                     help='Browser option needed: chrome, firefox')
+    parser.addoption('--language', action='store', default=None,
+                     help='User language option needed: ru, en, es, fr')
 
 @pytest.fixture(scope='function')
 def browser(request):
     browser_name = request.config.getoption('browser_name')
-    browser = None
-    if browser_name == 'chrome':
-        print('\nstarting Chrome browser')
-        options = webdriver.ChromeOptions()
-        options.add_experimental_option('excludeSwitches', ['enable-logging'])
-        browser = webdriver.Chrome(options=options)
-    elif browser_name == 'firefox':
-        print('\nstarting Firefox browser')
-        browser = webdriver.Firefox()
+    user_language = request.config.getoption('language')
+    if user_language:
+        if browser_name == 'chrome':
+            print('\nstarting Chrome browser')
+            options = webdriver.ChromeOptions()
+            options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+            options.add_experimental_option('excludeSwitches', ['enable-logging'])
+            browser = webdriver.Chrome(options=options)
+        elif browser_name == 'firefox':
+            print('\nstarting Firefox browser')
+            fp = webdriver.FirefoxProfile()
+            fp.set_preference("intl.accept_languages", user_language)
+            browser = webdriver.Firefox(firefox_profile=fp)
+        else:
+            raise pytest.UsageError('--browser_name should be chrome or firefox')
     else:
-        raise pytest.UsageError('--browser_name should be chrome or firefox')
+        raise pytest.UsageError('--language should be specified')
     yield browser
     print('\nquiting browser')
     browser.quit()
---
+++ test_items.py
@@ -0,0 +1,11 @@
+from selenium.webdriver.common.by import By
+from time import sleep
+
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
+
+def test_add_to_basket_button_should_be_available(browser):
+    browser.get(link)
+    browser.implicitly_wait(5)
+    browser.find_element(By.CLASS_NAME, 'btn-add-to-basket')
+    sleep(5)  # time to check the button name
